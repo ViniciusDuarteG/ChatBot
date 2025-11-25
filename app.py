cdbea1045655f3f856aa662c8838c3bbfc1ce46b
Vinicius Duarte <105383174+ViniciusDuarteG@users.noreply.github.com>
2025-11-25 15:45:54 -0400

    Enhance chatbot responses and interactions
    Updated response messages in the chatbot function.

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def Responder(pergunta):
     if "preço" in pergunta or "preco" in pergunta or "presso" in pergunta or "preso" in pergunta:
         return "De qual produto você gostaria de saber o preço?"
     
-    if "qual o seu objetivo" in pergunta: 
+    if "qual o seu objetivo" in pergunta or "faz" in pergunta: 
         return "Sou um chatbot demonstrativo, possuo uma quantidade limitada de interações. Posso responder dados, como uma pequena lista de produtos de casaque eu tenho, como uma cama, um sofá e uma mesa."
 
     if "desenvolveu" in pergunta or "desenvolvedor" in pergunta or "dev" in pergunta:
@@ -28,7 +28,8 @@ def Responder(pergunta):
 
     respostas = {
         "1": "Certo, como posso ajudar?",
-        "2": "Até logo!",
+        "2":"Sou um chatBot desenvolvido por Vinícius Duarte, meu objetivo é simular um atendimento real de um marketplace, porém posso ser utilizado para outros objetivos",
+        "0": "Até logo!",
         "bom dia": "Olá! Em que posso te ajudar?",
         "oi":"Olá! Em que posso te ajudar?",
         "olá":"Olá! Em que posso te ajudar?",
@@ -45,7 +46,7 @@ def index():
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    print("Olá, \n1 - Para iniciar o atendimento\n2- Encerrar o atendimento")
+    print("Olá, \n1 - Para iniciar o atendimento\n2 - Informações para o usuário\n0 - Encerrar o atendimento")
     dados = request.get_json()
     pergunta = dados.get("mensagem", "")
     resposta = Responder(pergunta)
